0774f44c08819ef092c13ba9af6bba7d02e4bf19
alphakahere <alphakahere@gmail.com>
2022-10-20 08:26:20 +0000

    add article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
@@ -16,10 +18,32 @@ class BlogController extends AbstractController
         ]);
     }
 
-    public function add()
+    public function add(Request $request)
     {
         $article = new Article();
         $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setLastUpdateDate(new \DateTime());
+            if ($article->getIsPublished()) {
+                $article->setPublicationDate(new \DateTime());
+            }
+            if($article->getPicture() !== null){
+               $file = $form->get('picture')->getData();
+               $filename = uniqid().'.'.$file->guessExtension();
+               try {
+                $file->move($this->getParameter('images_directory'), $filename);
+               } catch(FileException $e) {
+                return new Response($e->getMessage());
+               }
+            }
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return new Response('L\'article a bien ete enregistrer');
+
+        }
         return    $this->render('/blog/add.html.twig', [
             'form' => $form->createView(),
         ]);
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -6,6 +6,8 @@ use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass=ArticleRepository::class)
@@ -25,6 +27,7 @@ class Article
     private $picture;
 
     /**
+     * @Assert\NotBlank
      * @ORM\Column(type="string", length=255)
      */
     private $title;
